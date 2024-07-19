927a11fedae75b7a3704480f57f1648eb6aacf5c
Jake Richards <jrichca1@gmail.com>
2024-07-18 18:12:59 -0700

    show/hide password vaultentry

--- frontend/src/components/Vault/VaultEntry.vue
+++ frontend/src/components/Vault/VaultEntry.vue
@@ -4,6 +4,7 @@ import { decryptPassword } from "@/utils/Cryptography";
 const { entry } = defineProps(["entry"]);
 const showPassword = ref(false);
 const password = ref("");
+
 onMounted(async () => {
   password.value = await decryptPassword(entry.password, entry.iv);
 });
@@ -18,14 +19,13 @@ onMounted(async () => {
       <p>{{ entry.username }}</p>
     </div>
     <div class="vaultEntryPassword">
-      <button
-        v-if="!showPassword"
-        class="button is-primary"
-        @click="showPassword = true"
-      >
-        Show Password
+      <button class="button is-primary" @click="showPassword = !showPassword">
+        {{ showPassword ? "Hide Password" : "Show Password" }}
       </button>
-      <p v-else>{{ password }}</p>
+      <p v-if="showPassword">{{ password }}</p>
+      <p v-else>
+        <span v-for="i in password.length">*</span>
+      </p>
     </div>
   </div>
 </template>
