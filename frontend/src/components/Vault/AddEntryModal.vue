8127b9ecc5451cef32abf6a7ab65d99b7910daeb
Jake Richards <contact@jakerichards.info>
2024-08-13 16:05:40 -0700

    generate and options

--- frontend/src/components/Vault/AddEntryModal.vue
+++ frontend/src/components/Vault/AddEntryModal.vue
@@ -13,6 +13,26 @@ const handleClick = async () => {
   const modal = document.getElementById("add-entry-modal");
   if (modal) modal.classList.remove("is-active");
 };
+
+const length = ref(20);
+const numbers = ref(true);
+const symbols = ref(true);
+const uppercase = ref(true);
+const lowercase = ref(true);
+
+const generatePassword = () => {
+  const charset = [];
+  if (numbers.value) charset.push("0123456789");
+  if (symbols.value) charset.push("!@#$%^&*()_+-=[]{}|;:,.<>/?");
+  if (uppercase.value) charset.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  if (lowercase.value) charset.push("abcdefghijklmnopqrstuvwxyz");
+  password.value = "";
+  for (let i = 0; i < length.value; i++) {
+    const index = Math.floor(Math.random() * charset.length);
+    const charIndex = Math.floor(Math.random() * charset[index].length);
+    password.value += charset[index][charIndex];
+  }
+};
 </script>
 
 <template>
@@ -48,7 +68,32 @@ const handleClick = async () => {
           <button class="button" @click="hidden = !hidden">
             {{ hidden ? "Show" : "Hide" }}
           </button>
+          <button class="button" @click="generatePassword">Generate</button>
           <div class="control">
+            <div class="generateOptions">
+              <div class="lengthContainer">
+                <input type="range" v-model="length" min="8" max="64" />
+                <label>{{ length }}</label>
+              </div>
+              <div class="checkboxContainer">
+                <label class="checkbox">
+                  <input type="checkbox" v-model="numbers" />
+                  Numbers
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" v-model="symbols" />
+                  Symbols
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" v-model="uppercase" />
+                  Uppercase
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" v-model="lowercase" />
+                  Lowercase
+                </label>
+              </div>
+            </div>
             <input
               v-model="password"
               class="input"
